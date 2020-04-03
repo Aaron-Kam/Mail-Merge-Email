0d871e8fa5dd7b38347c734e089ed48973d64a94
Aaron-Kam <45779752+Aaron-Kam@users.noreply.github.com>
2020-04-02 14:52:01 -1000

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Mail-Merge-Email
- WIll take a mailing list from a google sheet and will email them based on a template
+Will take a mailing list from a google sheet and will email them based on a template
+
+
+# Instructions
+First you must take the sheet ID by going onto the sheet document, and look into its url. From there you will see that it is formatted as such:
+```
+https://docs.google.com/spreadsheets/d/SHEET_ID/edit#gid=0
+```
+
+Copy the sheet ID, and paste into the `SHEETS_FILE_ID` variable as a string. You are now able to pull from the spreadsheet! Congratulations.
+
+Create a text document called 'template.txt' in the working directory and write your message. Wherever you would like the program to insert data, simply put in a variable with the format `{{Variable Name}}` where variable name is the case sensitive name of the column in the google sheets. For example, if you have a table like so:
+| First Name  | Last Name  |  Email |
+|:--|:-:|--:|
+| Jonn  | Doe  | john@doe.com  |
+|  Jane | Who  |  jane@who.com |
+| Jake  | Lum  | lummyboi@gmail.com  |
+
+Then wherever in your template you want them to insert the recipients first name you would put `{{First Name}}` in the template. Any variables that do not corrrespond to a column in the spreadsheet will be left as is, and columns in the sheet that are not called will not be used.
+
+You must set a subject, which you can do by assigning it to the `SUBJECT` variable, and finally you must set your email. This can be done by changing the `SENDER` variable to your current email address. If it does not match your credentials, then it will fail.
+
+You can then run the program by typing
+```
+python email-sender.py
+```
+
+And if successful, it will print out the email IDs in the console.
---
+++ email-sender.py
@@ -0,0 +1,100 @@
+import base64
+from googleapiclient import discovery
+from httplib2 import Http
+from oauth2client import file, client, tools
+from email.mime.text import MIMEText
+
+def get_sheets_data(service):
+    """(private) Returns data from Google Sheets source. It gets all rows of
+        'Sheet1' (the default Sheet in a new spreadsheet), but drops the first
+        (header) row. Use any desired data range (in standard A1 notation).
+    """
+    headers = service.spreadsheets().values().get(spreadsheetId=SHEETS_FILE_ID,
+            range='Sheet1').execute().get('values')[0] # only header row 
+    recipient_info = service.spreadsheets().values().get(spreadsheetId=SHEETS_FILE_ID,
+            range='Sheet1').execute().get('values')[1:] # skip header row
+    
+    final_data = []
+    for person in recipient_info:
+        final_data.append(dict(zip(headers, person)))
+    return final_data
+
+def get_http_client():
+    """Uses project credentials in CLIENT_ID_FILE along with requested OAuth2
+        scopes for authorization, and caches API tokens in TOKEN_STORE_FILE.
+    """
+    store = file.Storage(TOKEN_STORE_FILE)
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_ID_FILE, SCOPES)
+        creds = tools.run_flow(flow, store)
+    return creds.authorize(Http())
+
+def create_message(sender, to, subject, message_text):
+  """Create a message for an email.
+
+  Args:
+    sender: Email address of the sender.
+    to: Email address of the receiver.
+    subject: The subject of the email message.
+    message_text: The text of the email message.
+
+  Returns:
+    An object containing a base64url encoded email object.
+  """
+  message = MIMEText(message_text)
+  message['to'] = to
+  message['from'] = sender
+  message['subject'] = subject
+  return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+def send_message(service, user_id, message):
+
+  """Send an email message.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    message: Message to be sent.
+
+  Returns:
+    Sent Message.
+  """
+  try:
+    message = (service.users().messages().send(userId=user_id, body=message)
+               .execute())
+    print('Message Id: %s' % message['id'])
+    return message
+  except(errors.HttpError, error):
+    print('An error occurred: %s' % error)
+
+
+CLIENT_ID_FILE = 'credentials.json'
+TOKEN_STORE_FILE = 'token.json'
+SCOPES = (  # iterable or space-delimited string
+    'https://www.googleapis.com/auth/spreadsheets.readonly',
+    'https://www.googleapis.com/auth/gmail.send'
+)
+
+HTTP = get_http_client()
+GMAIL = discovery.build('gmail', 'v1', http=HTTP)
+SHEETS = discovery.build('sheets', 'v4', http=HTTP)
+SHEETS_FILE_ID = 'INSERT SHEET ID HERE'
+SENDER = 'INSERT EMAIL ADDRESS'
+SUBJECT = 'This is a test'
+
+with open('template.txt', 'r') as file:
+    template = file.read()  
+
+
+
+data = get_sheets_data(SHEETS)
+for person in data:
+    message_body = template
+    for item in person:
+        search_str = '{{' + item + '}}'
+        message_body = message_body.replace(search_str, str(person[item]))
+    send_message(GMAIL, 'me', create_message(SENDER, person['Email'], SUBJECT, message_body))
+    
+
